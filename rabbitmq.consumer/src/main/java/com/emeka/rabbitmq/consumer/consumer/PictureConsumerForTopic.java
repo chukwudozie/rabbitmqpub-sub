05a34dcb71751ae17a6fc494347f98d26ba8288e
chukwudozie <emkychuks062@gmail.com>
2022-04-18 12:30:15 +0100

    add topic exchange producer and consumer

---
+++ rabbitmq.consumer/src/main/java/com/emeka/rabbitmq/consumer/consumer/PictureConsumerForTopic.java
@@ -0,0 +1,29 @@
+package com.emeka.rabbitmq.consumer.consumer;
+
+import com.emeka.rabbitmq.consumer.entity.Picture;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PictureConsumerForTopic {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PictureConsumerForTopic.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @RabbitListener(queues = {"q.picture.image", "q.picture.vector", "q.picture.log", "q.picture.filter"})
+    public void listen(Message messageAmqp) throws JsonProcessingException {
+        String message = new String(messageAmqp.getBody());
+        Picture picture = objectMapper.readValue(message, Picture.class);
+        LOGGER.info("Image : {} with routing key : {} ",
+                picture,messageAmqp.getMessageProperties().getReceivedRoutingKey());
+    }
+
+
+}
---
+++ rabbitmq.producer/src/main/java/com/emeka/rabbitmq/producer/producers/FurnitureProducer.java
@@ -0,0 +1,30 @@
+package com.emeka.rabbitmq.producer.producers;
+
+import com.emeka.rabbitmq.producer.entity.Furniture;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FurnitureProducer {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public  void sendMessage(Furniture furniture) throws JsonProcessingException {
+        MessageProperties properties = new MessageProperties();
+        properties.setHeader("color",furniture.getColor());
+        properties.setHeader("material", furniture.getMaterial());
+        String jsonFurniture = objectMapper.writeValueAsString(furniture);
+      Message message =  new Message(jsonFurniture.getBytes(),properties);
+
+      template.send("x.promotion","",message);
+    }
+}
